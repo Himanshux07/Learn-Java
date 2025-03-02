9304702c21b2cf13b25783061d8166cc0ca4df8f
Himanshux07 <annikumar997@gmail.com>
2025-03-02 22:39:18 +0530

    we have studied how to permutate string and count it

--- Recursion/src/permutation.java
+++ Recursion/src/permutation.java
@@ -1,7 +1,7 @@
 public class permutation {
     public static void main(String[] args) {
         permu("","abc");
-
+        System.out.println( countpermu("","abc"));
 
     }
 
@@ -17,5 +17,17 @@ public class permutation {
         }
     }
 
+    static int countpermu(String p,String up){
 
+        if(up.isEmpty()){
+            return 1;
+        }
+        int count=0;
+        for(int i=0;i<=p.length();i++){
+            String a=p.substring(0,i);
+            String b=p.substring(i);
+           count= count + countpermu(a+up.charAt(0)+b,up.substring(1));
+        }
+        return count;
+    }
 }
