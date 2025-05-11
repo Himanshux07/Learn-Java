d952958244ddb3c550b74e26e5abe11c675915d5
Himanshux07 <annikumar997@gmail.com>
2025-05-12 01:58:18 +0530

    ll

---
+++ LinkedList/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        LL list = new LL();
+
+        list.InsertFirstEle(4);
+        list.InsertFirstEle(5);
+        list.InsertFirstEle(6);
+        list.InsertFirstEle(7);
+
+
+        list.Display();
+
+    }
+}
